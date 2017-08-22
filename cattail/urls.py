5f6fb5bb6c7ade2dd86ceebadada78ea504f4142
adrian-turjak <adriant@catalyst.net.nz>
2017-08-22 21:26:09 +1200

    misc changes

--- cattail/urls.py
+++ cattail/urls.py
@@ -2,7 +2,6 @@ from __future__ import absolute_import, unicode_literals
 
 from django.conf import settings
 from django.conf.urls import include, url
-from django.contrib import admin
 
 from core import views as search_views
 from wagtail.wagtailadmin import urls as wagtailadmin_urls
@@ -10,8 +9,6 @@ from wagtail.wagtailcore import urls as wagtail_urls
 from wagtail.wagtaildocs import urls as wagtaildocs_urls
 
 urlpatterns = [
-    url(r'^django-admin/', include(admin.site.urls)),
-
     url(r'^admin/', include(wagtailadmin_urls)),
     url(r'^documents/', include(wagtaildocs_urls)),
 
